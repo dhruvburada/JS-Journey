00817a62b61f9eece41271513490da9bcef05cf1
Dhruv Burada <121245063+dhruvburada@users.noreply.github.com>
2024-05-17 15:26:28 +0000

    Data Types and Type Function

---
+++ basics/Data Types.js
@@ -0,0 +1,24 @@
+firstName="Dhruv"                                                                           
+lastName = "Burada"                                                        //String                 
+age = 18;                                                                              //Number                 
+bloodGroup = "A"                                                            //string
+weight = 70.2                                                                  //Number                 
+isMarried=false                                                           //Bolean      
+gf=null;                                                                        //Null
+nothing = NaN                                                       //Not a Number (is still type of number)
+let notProvided;                                                //Undefined
+let array = [1,2,3,4,5 ] ;                                   //Object Data Type
+
+
+
+console.log(firstName , "is type of", typeof(firstName));
+console.log(lastName," is type of ", typeof(lastName));
+console.log(age," is type of ", typeof age);
+console.log(bloodGroup,"is type of ", typeof bloodGroup)
+console.log(weight," is type of ",typeof weight)
+console.log(isMarried+" is type of ", typeof isMarried)
+console.log(gf+" is type of ", typeof gf)
+console.log(nothing+" is type of ", typeof nothing)
+console.log(notProvided + " is type of " +typeof(notProvided))
+console.log(array + " is type of " +typeof(array))
+
